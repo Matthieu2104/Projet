909c01cc1e4649665dddbd186297187d793f6e4c
Fesmaye-Junior <144659833+Fesmaye-Junior@users.noreply.github.com>
2024-03-22 09:55:33 +0100

    Add files via upload

---
+++ inscrit.php
@@ -0,0 +1,48 @@
+<?php
+// Inclusion du fichier de configuration
+require_once('config2.php');
+
+// Vérification si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupération des champs du formulaire
+    $username = $_POST['username'];
+    $mail = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Définition du grade par défaut
+    $grade = "member";
+
+    // Connexion à la base de données
+    $pdo = get_pdo_instance();
+
+    try {
+        // Préparation de la requête d'insertion
+        $stmt = $pdo->prepare("INSERT INTO user.inscrit (username, grade, password, mail) VALUES (:username, :grade, :password, :mail)");
+
+        // Liaison des valeurs aux paramètres de la requête
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':grade', $grade);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':mail', $mail);
+
+        // Exécution de la requête
+        $stmt->execute();
+
+        // Ajout d'un message de succès à la réponse JSON
+        $response['success'] = true;
+        $response['message'] = "inscription valider";
+
+    } catch (PDOException $e) {
+        // En cas d'erreur, ajout d'un message d'erreur à la réponse JSON
+        $response['success'] = false;
+        $response['message'] = "Erreur lors de l'insertion dans la base de données: " . $e->getMessage();
+    }
+} else {
+    // Si la méthode de la requête n'est pas POST, ajout d'un message d'erreur à la réponse JSON
+    $response['success'] = false;
+    $response['message'] = "Méthode de requête incorrecte.";
+}
+
+// Conversion du tableau en JSON
+echo json_encode($response);
+?>

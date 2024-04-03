13287823edf5d46f95af0700e40b9e7f93df4d35
Fesmaye-Junior <144659833+Fesmaye-Junior@users.noreply.github.com>
2024-04-03 11:10:49 +0200

    Add files via upload

---
+++ gererCadenas.php
@@ -0,0 +1,29 @@
+<?php
+include 'config2.php';
+
+$pdo = get_pdo_instance();
+
+// Vérifier la connexion
+if (!$pdo) {
+    die("Connection failed: " . $pdo->errorInfo()[2]); // Affiche l'erreur de connexion
+}
+
+// Requête SQL pour récupérer les données
+$sql = "SELECT id,grade, adresseMac FROM fablab2024.cadenas";
+$result = $pdo->query($sql);
+
+$data = array();
+
+if ($result) {
+    // Ajouter chaque ligne de données à un tableau
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $data[] = $row;
+    }
+}
+
+// Renvoyer les données au format JSON
+echo json_encode($data);
+
+// Fermer la connexion à la base de données
+$pdo = null;
+?>
---
+++ supprimerCadena.php
@@ -0,0 +1,31 @@
+<?php
+// Connexion à la base de données
+include 'config2.php';
+
+$pdo = get_pdo_instance();
+
+
+// Vérifier si l'ID à supprimer est passé en paramètre
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Requête pour supprimer l'enregistrement avec l'ID spécifié
+    $sql = "DELETE FROM fablab2024.cadenas WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+    if ($stmt->execute()) {
+        // Succès de la suppression
+        echo json_encode(array("message" => "L'enregistrement a ete supprime avec succes."));
+    } else {
+        // Erreur lors de la suppression
+        echo json_encode(array("error" => "Erreur lors de la suppression de l'enregistrement: " ));
+    }
+} else {
+    // Aucun ID spécifié
+    echo json_encode(array("error" => "Aucun ID spécifié pour la suppression."));
+}
+
+// Fermer la connexion à la base de données
+$pdo = null;
+?>

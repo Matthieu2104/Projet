e96651a77e4b90eda0877f0d026a682acfc9bb64
thomasDS <144811624+ThomasD-S@users.noreply.github.com>
2024-05-07 16:59:11 +0200

    V2

--- TestCodeSimpleEnvoie.ino
+++ TestCodeSimpleEnvoie.ino
@@ -1,4 +1,6 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "E5576_93F9";
 const char* password = "inagd6TbhmB";
@@ -31,34 +33,41 @@ void setup() {
 void loop() {
   // Exemple d'envoi de données à votre fichier PHP
   sendToPHP("Test envoie");
-  delay(5000); // Attendez 5 secondes avant d'envoyer les prochaines données
+  delay(5000); 
 }
 
-void sendToPHP(String data) {
+void sendToPHP() {
   WiFiClient client;
 
-  if (client.connect(serverAddress, serverPort)) {
-    Serial.println("Connexion établie avec le serveur PHP");
+  HTTPClient http;
 
-    client.print("POST /btssnir/projets2024/fablab2024/fablab2024/site/projetApiXweb/recupArduino.php HTTP/1.1\r\n");
-    client.print("Host: ");
-    client.println(serverAddress);
-    client.println("Connection: close");
-    client.println("Content-Type: text/plain");
-    client.print("Content-Length: ");
-    client.println(data.length());
-    client.println();
-    client.println(data);
+  // Créez un objet JSON
+  DynamicJsonDocument jsonDoc(200); // Taille du document JSON
+  jsonDoc["key"] = "value"; // Ajoutez des données à l'objet JSON
+
+  // Convertissez l'objet JSON en une chaîne JSON
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
+
+  if (http.begin(client, "http://" + String(serverAddress) + ":" + String(serverPort)  + "/btssnir/projets2024/fablab2024/fablab2024/site/projetApiXweb/recupArduino.php")) {
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.println("JSON envoyé : " + jsonString);
+
+    int httpResponseCode = http.POST(jsonString);
+
+    if (httpResponseCode > 0) {
+      Serial.print("Code de réponse HTTP: ");
+      Serial.println(httpResponseCode);
+      String response = http.getString();
+      Serial.println(response);
+    } else {
+      Serial.print("Échec de la requête HTTP : ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
   } else {
     Serial.println("Échec de la connexion avec le serveur PHP");
   }
-
-  while (client.connected()) {
-    if (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
-  }
-  Serial.println();
-  Serial.println("Déconnecté du serveur");
 }

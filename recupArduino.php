f5ba2fde48ff10e2843484325a39d6c0519fb07f
Fesmaye-Junior <144659833+Fesmaye-Junior@users.noreply.github.com>
2024-05-22 09:43:51 +0200

    Update recupArduino.php

--- recupArduino.php
+++ recupArduino.php
@@ -7,17 +7,12 @@ require_once('config2.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérez le corps de la requête HTTP
     $data = json_decode(file_get_contents('php://input'), true);
-
-    // Inclusion du fichier de configuration
-    require_once('config2.php');
     
     // Vérifiez si les données sont présentes et non vides
-    if (isset($data['MacAddress']) && isset($data['CardID']) && isset($data['DateTime'])) {
+    if (isset($data['MacAddress']) && isset($data['CardID'])) {
         // Récupérez les données
         $adresseMac = $data['MacAddress'];
         $numero = $data['CardID'];
-        $dateTime = $data['DateTime'];
-    
 
         // Initialiser la variable pour stocker le résultat de la connexion
         $valide = "non";
@@ -29,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Utilisation de requêtes préparées pour éviter les injections SQL
             $cadenaGrade = "SELECT grade FROM fablab2024.cadenas WHERE adresseMac = :adresseMac";
             $carteGrade = "SELECT grade FROM fablab2024.inscrit WHERE numero = :numero";
+            $idQuery = "SELECT id FROM fablab2024.inscrit WHERE numero = :numero";
 
             $cadenaRe = $pdo->prepare($cadenaGrade);
             $cadenaRe->bindParam(':adresseMac', $adresseMac, PDO::PARAM_STR);
@@ -38,12 +34,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $carteRe->bindParam(':numero', $numero, PDO::PARAM_STR);
             $carteRe->execute();
 
+            $idRe = $pdo->prepare($idQuery);
+            $idRe->bindParam(':numero', $numero, PDO::PARAM_STR);
+            $idRe->execute();
+
             $cadena = $cadenaRe->fetch(PDO::FETCH_ASSOC);
             $carte = $carteRe->fetch(PDO::FETCH_ASSOC);
+            $inscrit_id = $idRe->fetch(PDO::FETCH_ASSOC);
 
             if ($carte && $cadena) {
                 $gradesAllowed = [
-                    "member" => ["member"],
                     "teacher" => ["member", "teacher"],
                     "manager" => ["member", "teacher", "manager"],
                     "admin" => ["member", "teacher", "manager", "admin"]
@@ -51,8 +51,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 if (in_array($cadena["grade"], $gradesAllowed[$carte["grade"]])) {
                     $valide = "ok";
+
+                    // Récupérer les détails actuels
+                    $annee = date('Y');
+                    $mois = date('m');
+                    $jour = date('d');
+                    $heure = date('H');
+                    $minutes = date('i');
+
+                    $logincre = "INSERT INTO fablab2024.logs (anne, mois, jour, heure, minutes, inscrit_id) VALUES (:anne, :mois, :jour, :heure, :minutes, :inscrit_id)";
+                    $logRE = $pdo->prepare($logincre);
+
+                    $logRE->bindParam(':anne', $annee, PDO::PARAM_INT);
+                    $logRE->bindParam(':mois', $mois, PDO::PARAM_INT);
+                    $logRE->bindParam(':jour', $jour, PDO::PARAM_INT);
+                    $logRE->bindParam(':heure', $heure, PDO::PARAM_INT);
+                    $logRE->bindParam(':minutes', $minutes, PDO::PARAM_INT);
+                    $logRE->bindParam(':inscrit_id', $inscrit_id['id'], PDO::PARAM_INT);
+
+                    $logRE->execute();
                 }
             }
+
         } catch (PDOException $e) {
             // Gestion des erreurs de base de données
             echo json_encode(array("grade_valide" => false, "message" => "Erreur de base de données: " . $e->getMessage()));
@@ -76,4 +96,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode(array("grade_valide" => false, "message" => "Données manquantes ou invalides."));
     }
 }
-    ?>
+?>
+
